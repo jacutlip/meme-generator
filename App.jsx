c380285ad68e4cc5159da4844a6e3f21910288fd
jacutlip <josephcutlip@gmail.com>
2025-05-06 17:38:42 -0500

    initial commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import Main from "./components/main"
+
+export default function App() {
+    
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
